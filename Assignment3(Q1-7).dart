fb46d3e5b9010591521c80271b06231d2c310474
AhmedAmin <ahmedamin1024@gmail.com>
2022-03-12 21:50:50 +0500

    Task Complete

---
+++ Assignment3(Q1-7).dart
@@ -0,0 +1,149 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  var a = 51;
+  var b = 17;
+  var c = 51;
+  var d = 17;
+  print("====================Question 1===================");
+  print("---------------------------------------");
+  print("||Arithematic Operators||");
+
+  var add = a + b;
+  var sub = a - b;
+  var mul = a * b;
+  var div = a / b;
+
+  print("+ Operator $add");
+  print("- Operator $sub");
+  print("* Operator $mul");
+  print("/ Operator $div");
+
+  print("---------------------------------------");
+
+  print("||Equality Operators||");
+
+  if (a > b) {
+    print("$a is greater than $b");
+  }
+  if (a < b) {
+    print("$b is less than $a");
+  }
+  if (a >= b) {
+    print("$a is greater than equal to $b");
+  }
+  if (a <= b) {
+    print("$b is less than equal to $a");
+  }
+  if (a == b) {
+    print("$a is equal to $b");
+  }
+  if (a != b) {
+    print("$a is not equal to $b");
+  }
+  print("---------------------------------------");
+
+  print("||Logical Operators||");
+  print("AND Operator");
+  if ((a == c) && (b == d)) {
+    print("AND FALLS TRUE!!!");
+  }
+  print("OR Operator");
+  if ((a == c) || (b > d)) {
+    print("OR FALLS TRUE!!!");
+  }
+  print("NOT (!) Operator");
+  if (!((a == c) || (b > d))) {
+    print("OR FALLS FALSE!!!");
+  }
+  print("=================================================");
+  print("====================Question 2===================");
+
+  num cost = 600;
+  num costof5 = cost * 5;
+  print("Cost Of 5 Tickets To a MOVIE is PKR $costof5");
+
+  print("=================================================");
+  print("====================Question 3===================");
+  print(
+      "(Q3)How to get difference of lists in Dart? Problem: Consider you have two lists [1,2,3,4,5,6,7] and [3,5,6,7,9,10]. How would you get the difference as output? E.g. [1, 2, 4].");
+
+  dynamic list1 = [1, 2, 3, 4];
+  dynamic list2 = [3, 5, 6, 7];
+  dynamic sublist = [];
+  var i = 0;
+
+  while (i <= 3) {
+    var listsub = list2[i] - list1[i];
+    sublist.add(listsub);
+    i = i + 1;
+  }
+  print("List 1 : $list1");
+  print("List 2 : $list2");
+
+  print("Result : $sublist");
+  print("=================================================");
+  print("====================Question 4===================");
+  // THE DIFFERENCE BETWEEN THE "??" AND "?" IS :
+  // condition ? exp1 : exp2
+  // If condition is true, then the expression evaluates expr1 (and returns its value); otherwise, it evaluates and returns the value of expr2.
+
+  // exp1 ?? exp2
+  // If expr1 is non-null, returns its value; otherwise, evaluates and returns the value of expr2
+  print("=================================================");
+
+  print("====================Question 5===================");
+  print("----------------Number(num) Data Type-------------");
+
+  print("----------------String(String) Data Type-------------");
+  num number1 = 5;
+  print("Number = $number1");
+  sublist.add(number1);
+  String string1 = "Hello";
+  print("String = $string1");
+  sublist.add(string1);
+  print("----------------Boolean(bool) Data Type-------------");
+  bool boolvalue = true;
+  if (boolvalue == true) {
+    print("List : $sublist");
+    boolvalue = false;
+  }
+  print("=================================================");
+  print("====================Question 6===================");
+
+  dynamic list3 = [7, 14, 21, 28, 35, 42, 49, 56, 63, 70];
+  dynamic list4 = [
+    '7 x 1',
+    '7 x 2',
+    '7 x 3',
+    '7 x 4',
+    '7 x 5',
+    '7 x 6',
+    '7 x 7',
+    '7 x 8',
+    '7 x 9',
+    '7 x 10'
+  ];
+
+  Map data = Map.fromIterables(list4, list3);
+  print("The Table of 7 is (Times: Answer) $data");
+  print("=================================================");
+  print("====================Question 7===================");
+  //Login Credentials Telling Whick Credentials was wrong
+  var real_pass = "SlickNick1024";
+  bool t = true;
+  while (t) {
+    print("-------------------------------");
+    print("Please Enter your password: ↓↓↓");
+    var password = stdin.readLineSync();
+    print("-------------------------------");
+
+    if (real_pass == password) {
+      print("Correct! The Pasword you Entered Matches the Real Password");
+      print("--------------------------------");
+      return;
+    } else {
+      print("Incorrect Password");
+    }
+  }
+}
---
+++ Assignment3(Q8-13).dart
@@ -0,0 +1,101 @@
+void main(List<String> args) {
+  print("=================================================");
+  print("====================Question 8===================");
+  dynamic name = ['Ahmed', 'Amin', 'Gilani'];
+  dynamic marksObt = [450, 479, 397];
+  dynamic percentages = [];
+
+  num i = 0;
+  while (i <= 2) {
+    num percentage = ((marksObt[i]) / 500) * 100;
+    percentages.add(percentage);
+    i = i + 1;
+  }
+  var marksheet = Map.fromIterables(name, percentages);
+  print(marksheet);
+  print("=================================================");
+  print("====================Question 9===================");
+  // print("-------------Legal Variables---------------------");
+  // num var1 = 16;
+  // var var2 = 17;
+  // String var3 = "awfqf";
+  // bool var4 = true;
+  // Map var5 = {1: "hello", 2: "Hello", 3: "hEllo"};
+  // print("---------------Legal Variables-------------------");
+  // num var1 = {1: "hello", 2: "Hello", 3: "hEllo"};
+  // bool var2 = "HAawfq";
+  // String var3 = false;
+  // bool var4 = 17;
+  // Map var5 = 16;
+  print("=================================================");
+  print("====================Question 10===================");
+
+  String str = 'Hyderabad';
+
+  var result = str.replaceAll("Hyder", "Islam");
+  print(result);
+  print("=================================================");
+  print("====================Question 11===================");
+  var customerName = "Ahmed Amin";
+  List months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+  ];
+  var now = new DateTime.now();
+  var current_mon = now.month;
+  var currentMonth = months[current_mon - 1] + ", " + now.year.toString();
+  var dueDate = (now.day + 10).toString() + " " + currentMonth.toString();
+  var noUnits = 300;
+  var chargesPerUnit = 4.5;
+  var amountPayable = noUnits * 4.5;
+  var latePaymentCharge = (amountPayable / 100) * 5;
+  var grossAmountPayable = amountPayable + latePaymentCharge;
+  print("-----------------");
+  print("K-Electric");
+  print("-----------------");
+  print("Customer Name: " + customerName);
+  print("Billing Month: " + currentMonth);
+  print("Total Units: " + noUnits.toString());
+  print("Charges Per Unit: Rs. " + chargesPerUnit.toStringAsFixed(2) + "/unit");
+  print("Net Amount Payable: Rs. " + amountPayable.toStringAsFixed(2));
+  print("Due Date: " + dueDate);
+  print("Late Payment Surcharge: Rs. " + latePaymentCharge.toStringAsFixed(2));
+  print("Gross Payment Payable: Rs. " + grossAmountPayable.toStringAsFixed(2));
+
+  print("=================================================");
+  print("====================Question 13===================");
+  List<dynamic> myList = [
+    "Syed",
+    "AHMED",
+    "Amin",
+    "Gilani",
+    "Pasha",
+    10,
+    23,
+    30
+  ];
+  print("List: $myList");
+  print("SubList: ");
+  print(myList.sublist(1, 3));
+  print("Shuffle: ");
+  myList.shuffle();
+  print(myList);
+  print("AsMap: ");
+  Map<int, dynamic> map = myList.asMap();
+  print(map);
+  print("whereType: (int)");
+  var whereList = myList.whereType<int>();
+  print(whereList);
+  print("take(): ");
+  print(myList.take(2));
+}
